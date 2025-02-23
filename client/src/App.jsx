9a411393e3471f0f99a1172ed5e80dbcb2b74610
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-02-23 10:36:22 +0200

    Initial project setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
+import TodoList from './components/TodoList'
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      {/* <!-- Navigation header --> */}
+      <header className="navigation-header">
+        <span className="navigation-logo">
+          <img src="/images/todo-icon.png" alt="todo-logo" />
+        </span>
+        <span className="spacer"></span>
+        <span className="navigation-description">Todo List</span>
+      </header>
+
+      {/* <!-- Main content --> */}
+
+      <main className="main">
+        <TodoList />
+      </main>
+
+
+      {/* <!-- Footer --> */}
+      <footer className="footer">
+        <p>Copyright © designed by Mihail Valkov</p>
+      </footer>
     </>
   )
 }
